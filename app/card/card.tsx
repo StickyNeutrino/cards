89a2922ab02cb6fcdf128268892c824cd519992a
Daniel Fitchmun <daniel@unimpossy.com>
2025-11-14 13:25:55 -0800

    remove animation for instant flips

--- app/card/card.tsx
+++ app/card/card.tsx
@@ -11,7 +11,7 @@ export function Card({card, invasive, flipped, widthRef, flipSpeed}:CardProps) {
     return (
         <div className="card-area" ref={widthRef}>
             <div className={`flip-card ${flipped ? "flipped" : "flip-card-enabled"}`} style={{ '--flip-speed': `${flipSpeed}s` } as React.CSSProperties}>
-                <div className="flip-card-inner">
+                <div className={`flip-card-inner ${flipSpeed === 0 ? "" : "flip-card-inner-animated"}`}>
                     {card && <img className="flip-card-front" src={`/cards/${card} Front.png`}/>}
                     {card && <img className={`flip-card-back ${invasive ? "invasive" : ""}`} src={`/cards/${card} Back.png`}/>}
                 </div>
