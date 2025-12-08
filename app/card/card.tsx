beb2c2c6feb23ce6b26dd4cb580fcd94af1d35dc
Daniel Fitchmun <daniel@unimpossy.com>
2025-12-08 04:07:25 +0000

    fix card flip back

--- app/card/card.tsx
+++ app/card/card.tsx
@@ -11,12 +11,12 @@ interface CardProps {
 export function Card({card, flipped, widthRef, flipSpeed, onClick}:CardProps) {
     const invasive = invasives.includes(card);
     
-    const handleClick = (e: React.MouseEvent) => {
+    const handleClick = (e: React.SyntheticEvent) => {
         e.stopPropagation();
         onClick?.();
     };
     return (
-        <div className="card-area" ref={widthRef} data-testid="card" data-card={card} data-flipped={flipped} data-invasive={invasive} onClick={handleClick}>
+        <div className="card-area" ref={widthRef} data-testid="card" data-card={card} data-flipped={flipped} data-invasive={invasive} onClick={handleClick} onTouchEnd={handleClick}>
             <div className={`flip-card ${flipped ? "flipped" : "flip-card-enabled"}`} style={{ '--flip-speed': `${flipSpeed}s` } as React.CSSProperties}>
                 <div className={`flip-card-inner ${flipSpeed === 0 ? "" : "flip-card-inner-animated"}`}>
                     {card && <img className="flip-card-front" src={`/cards/${card} Front.png`}/>}
